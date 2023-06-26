8daec3325b706a495837694100b7625741dda0eb
lucasolisouza <trabalgoritmo2@gmail.com>
2023-06-26 19:48:25 -0300

    feat-ready-v2.0

--- FTPClient.java
+++ FTPClient.java
@@ -1,196 +1,171 @@
 import java.io.*;
 import java.net.Socket;
-import java.nio.file.Paths;
-import java.nio.file.Path;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class FTPClient {
-    private static final String DEFAULT_DOWNLOAD_DIRECTORY = "C:\\Users\\lucas\\Documents\\RedesDeComputadoresI\\Client\\"; // Defina o diretório padrão para salvar os arquivos recebidos
+    private static final String DEFAULT_DOWNLOAD_DIRECTORY = "C:\\Users\\lucas\\Documents\\Sistemais Operacionais\\FTPApplication\\"; // Defina o diretório padrão para salvar os arquivos recebidos
 
-    public static void main(String[] args) {
-        String serverAddress = "localhost";
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        String serverAddress = "26.132.1.15";
         int controlPort = 12384;
         int dataPort = 8888;
+        Socket controlSocket = new Socket(serverAddress, controlPort);
 
-        // Laço para receber e executar funções
-        try{
-            Socket controlSocket = new Socket(serverAddress, controlPort);
+        String userInput;
+        do {
+			System.out.print("ftp> ");
+			Scanner ler = new Scanner(System.in);
+			userInput =ler.nextLine();
+			String[] comando = userInput.split(" "); 
+			
+			switch (comando[0]) {
+			case "LIST": 
+				listFiles(controlSocket,serverAddress, controlPort, dataPort);
+				break;
+			case "RETR": 
+				retrieveFile(controlSocket,serverAddress, controlPort, dataPort, comando[1], DEFAULT_DOWNLOAD_DIRECTORY);
+				break;
+			case "STOR":
+				storeFile(controlSocket,serverAddress, controlPort, dataPort, comando[1], comando[2]);
+				break;
+			case "PWD":
+				printWorkingDirectory(controlSocket);
+				break;
+			case "CWD":
+				changeWorkingDirectory(controlSocket, comando[1]);
+				break;
+			case "CDUP":
+				changetoParentDirectory(controlSocket);
+				break;
+			case "QUIT":
+				System.out.println("Goodbye...");
+				break;
+			default:
+				throw new IllegalArgumentException("Comando " + userInput+" não reconhecido.");
+			}	
+        } while (!userInput.equals("QUIT"));
+        try {controlSocket.close();}
+        catch (IOException e) {e.printStackTrace();}
+        
+    }
+
+    public static void retrieveFile(Socket controlSocket, String serverAddress, int controlPort, int dataPort, String filename, String downloadDirectory) {
+        try {
             PrintWriter controlWriter = new PrintWriter(controlSocket.getOutputStream(), true);
             BufferedReader controlReader = new BufferedReader(new InputStreamReader(controlSocket.getInputStream()));
 
-            boolean running = true;
-            while (running) {
-                BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
-                System.out.print("ftp> ");
-                String input = consoleReader.readLine();
-                String[] commandParts = input.split(" ");
-                String command = commandParts[0];
-
-                switch (command) {
-                    case "RETR":
-                        if (commandParts.length > 1) {
-                            String filename = commandParts[1];
-                            retrieveFile(controlWriter, controlReader, dataPort, filename, 
-                                        serverAddress, controlPort, DEFAULT_DOWNLOAD_DIRECTORY);
-                        } else {
-                            System.out.println("Comando RETR requer um nome de arquivo.");
-                        }
-                        break;
-
-                    case "LIST":
-                        listFiles(controlWriter, controlReader, dataPort, serverAddress, controlPort);
-                        //listFiles(serverAddress, controlPort, dataPort);
-                        break;
-
-                    case "PWD":
-                        printWorkingDirectory(serverAddress, controlPort);
-                        break;
-
-                    case "STOR":
-                        if (commandParts.length > 2) {
-                            String filename = commandParts[1];
-                            String directory = commandParts[2];
-                            storeFile(serverAddress, controlPort, dataPort, filename, directory);
-                        } else {
-                            System.out.println("Comando STOR requer um nome de arquivo e um diretório de destino.");
-                        }
-                        break;
-                    
-                    case "CDW":
-                        break;
-
-                    case "QUIT":
-                        running = false;
-                        break;
-
-                    default:
-                        System.out.println("Comando inválido.");
-                }
-            }
-            controlSocket.close();
-        }catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void retrieveFile(PrintWriter controlWriter, BufferedReader controlReader, int dataPort, String filename,
-                                    String serverAddress, int controlPort, String downloadDirectory ) {
-        try {
             controlWriter.println("RETR " + filename);
             String response = controlReader.readLine();
 
             if (response.equals("OK")) {
                 int serverDataPort = Integer.parseInt(controlReader.readLine());
-
                 Socket dataSocket = new Socket(serverAddress, serverDataPort);
                 BufferedInputStream dataReader = new BufferedInputStream(dataSocket.getInputStream());
                 FileOutputStream fileWriter = new FileOutputStream(downloadDirectory + filename);
-
                 byte[] buffer = new byte[1024];
                 int bytesRead;
-
-                while ((bytesRead = dataReader.read(buffer)) != -1) {
+                while ((bytesRead = dataReader.read(buffer)) != -1)
                     fileWriter.write(buffer, 0, bytesRead);
-                }
-
                 fileWriter.close();
                 dataReader.close();
                 dataSocket.close();
-
                 System.out.println("Arquivo '" + filename + "' recebido e salvo em: " + downloadDirectory);
-            } else {
-                System.out.println("Erro: " + response);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            } else System.out.println("Erro: " + response);
+        } catch (IOException e) {e.printStackTrace();}
     }
 
-    public static void listFiles(PrintWriter controlWriter, BufferedReader controlReader, int dataPort,String serverAddress, int controlPort) {
+    public static void listFiles(Socket controlSocket,String serverAddress, int controlPort, int dataPort) {
         try {
+            PrintWriter controlWriter = new PrintWriter(controlSocket.getOutputStream(), true);
+            BufferedReader controlReader = new BufferedReader(new InputStreamReader(controlSocket.getInputStream()));
+
             controlWriter.println("LIST");
             String response = controlReader.readLine();
-
             if (response.equals("OK")) {
                 int serverDataPort = Integer.parseInt(controlReader.readLine());
-
                 Socket dataSocket = new Socket(serverAddress, serverDataPort);
                 BufferedReader dataReader = new BufferedReader(new InputStreamReader(dataSocket.getInputStream()));
 
                 String line;
-                while ((line = dataReader.readLine()) != null) {
+                while ((line = dataReader.readLine()) != null)
                     System.out.println(line);
-                }
-
                 dataReader.close();
                 dataSocket.close();
-
                 System.out.println("Lista de arquivos recebida com sucesso.");
-            } else {
-                System.out.println("Erro: " + response);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            } else System.out.println("Erro: " + response);
+        } catch (IOException e) {e.printStackTrace();}
     }
 
-    public static void printWorkingDirectory(String serverAddress, int controlPort) {  //TODO: retirar o socket de dentro da função.
+    public static void printWorkingDirectory(Socket controlSocket) {
         try {
-            Socket controlSocket = new Socket(serverAddress, controlPort);
             PrintWriter controlWriter = new PrintWriter(controlSocket.getOutputStream(), true);
             BufferedReader controlReader = new BufferedReader(new InputStreamReader(controlSocket.getInputStream()));
 
             controlWriter.println("PWD");
             String response = controlReader.readLine();
-
             if (response.equals("OK")) {
                 String currentDirectory = controlReader.readLine();
                 System.out.println("Diretório atual: " + currentDirectory);
-            } else {
-                System.out.println("Erro: " + response);
-            }
+            } else System.out.println("Erro: " + response);
+        } catch (IOException e) {e.printStackTrace();}
+    }
+    
+    public static void changeWorkingDirectory(Socket controlSocket, String comando) {
+        try {
+            PrintWriter controlWriter = new PrintWriter(controlSocket.getOutputStream(), true);
+            BufferedReader controlReader = new BufferedReader(new InputStreamReader(controlSocket.getInputStream()));
 
-            controlSocket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            controlWriter.println("CWD "+comando);
+            String response = controlReader.readLine();
+            if (response.equals("OK")) {
+                String currentDirectory = controlReader.readLine();
+                System.out.println("Diretório atual: " + currentDirectory);
+            } else System.out.println("Erro: " + response);
+        } catch (IOException e) {e.printStackTrace();}
     }
 
-    public static void storeFile(String serverAddress, int controlPort, int dataPort, String filename, String directory) { //TODO: retirar o socket de dentro da função.
+    public static void changetoParentDirectory(Socket controlSocket) {
+    	  try {
+              PrintWriter controlWriter = new PrintWriter(controlSocket.getOutputStream(), true);
+              BufferedReader controlReader = new BufferedReader(new InputStreamReader(controlSocket.getInputStream()));
+
+              controlWriter.println("CDUP");
+              String response = controlReader.readLine();
+              if (response.equals("OK")) {
+                  String currentDirectory = controlReader.readLine();
+                  System.out.println("Diretório Alterado com sucesso \nDiretório atual: " + currentDirectory);
+              } else System.out.println("Erro: " + response);
+          } catch (IOException e) {e.printStackTrace();}
+    }
+    
+    
+    public static void storeFile(Socket controlSocket, String serverAddress, int controlPort, int dataPort, String filename, String directory) { //TODO: retirar o socket de dentro da função.
         try {
-            Socket controlSocket = new Socket(serverAddress, controlPort);
             PrintWriter controlWriter = new PrintWriter(controlSocket.getOutputStream(), true);
             BufferedReader controlReader = new BufferedReader(new InputStreamReader(controlSocket.getInputStream()));
 
             controlWriter.println("STOR " + filename + " " + directory);
             String response = controlReader.readLine();
-
             if (response.equals("OK")) {
                 // Cria um soquete de dados para enviar o arquivo
-                Socket dataSocket = new Socket(serverAddress, dataPort);
+            	int serverPort = Integer.parseInt(controlReader.readLine());
+                Socket dataSocket = new Socket(serverAddress, serverPort);
                 BufferedOutputStream dataWriter = new BufferedOutputStream(dataSocket.getOutputStream());
-
-                FileInputStream fileReader = new FileInputStream(filename);
+                File arquivo = new File(DEFAULT_DOWNLOAD_DIRECTORY+filename);
+                FileInputStream fileReader = new FileInputStream(arquivo);
                 byte[] buffer = new byte[1024];
                 int bytesRead;
 
-                while ((bytesRead = fileReader.read(buffer)) != -1) {
+                while ((bytesRead = fileReader.read(buffer)) != -1)
                     dataWriter.write(buffer, 0, bytesRead);
-                }
-
                 fileReader.close();
                 dataWriter.flush();
                 dataWriter.close();
                 dataSocket.close();
-
                 System.out.println("Arquivo '" + filename + "' enviado para o servidor");
-            } else {
-                System.out.println("Erro: " + response);
-            }
+            } else System.out.println("Erro: " + response);
 
-            controlSocket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        } catch (IOException e) {e.printStackTrace();}
     }
-
 }
--- FTPServer.java
+++ FTPServer.java
@@ -1,10 +1,13 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.List;
 
 public class FTPServer {
-    public static final String DEFAULT_DIRECTORY = "C:\\Users\\lucas\\Documents\\RedesDeComputadoresI\\Server\\"; // Defina o diretório padrão para os arquivos
+    public static final String DEFAULT_DIRECTORY = "\\diretory\\"; // Defina o diretório padrão para os arquivos
 
     public static void main(String[] args) {
         int controlPort = 12384;
@@ -20,134 +23,114 @@ public class FTPServer {
                 Thread clientThread = new Thread(new ClientHandler(clientSocket, dataPort));
                 clientThread.start();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        } catch (IOException e) {e.printStackTrace();}
     }
 }
 
 class ClientHandler implements Runnable {
     private Socket controlSocket;
     private int dataPort;
-    private String currentDirectory;
+    public String currentDirectory;
 
     public ClientHandler(Socket controlSocket, int dataPort) {
         this.controlSocket = controlSocket;
         this.dataPort = dataPort;
-        this.currentDirectory = FTPServer.DEFAULT_DIRECTORY;
+        this.currentDirectory = ServerTESTE.DEFAULT_DIRECTORY;
     }
 
     public void run() {
         try {
             String clientAddress = controlSocket.getInetAddress().getHostAddress();
             System.out.println("Conexão estabelecida com " + clientAddress);
-
+            File dirAtual = new File(FTPServer.DEFAULT_DIRECTORY);
             BufferedReader controlReader = new BufferedReader(new InputStreamReader(controlSocket.getInputStream()));
             PrintWriter controlWriter = new PrintWriter(controlSocket.getOutputStream(), true);
-
-            String command = controlReader.readLine();
-            String[] commandParts = command.split(" ");
-
-            if (commandParts[0].equals("RETR")) {
-                String filename = commandParts[1];
-                File file = new File(FTPServer.DEFAULT_DIRECTORY + filename);
-
-                if (file.exists()) {
-                    controlWriter.println("OK");
-
-                    // Cria um soquete de dados para o cliente
-                    ServerSocket dataSocket = new ServerSocket(dataPort);
-                    controlWriter.println(dataPort);
-
-                    Socket dataConnection = dataSocket.accept();
-                    BufferedOutputStream dataWriter = new BufferedOutputStream(dataConnection.getOutputStream());
-
-                    FileInputStream fileReader = new FileInputStream(file);
-                    byte[] buffer = new byte[1024];
-                    int bytesRead;
-
-                    while ((bytesRead = fileReader.read(buffer)) != -1) {
-                        dataWriter.write(buffer, 0, bytesRead);
-                    }
-
-                    fileReader.close();
-                    dataWriter.flush();
-                    dataWriter.close();
-                    dataConnection.close();
-                    dataSocket.close();
-
-                    System.out.println("Arquivo '" + filename + "' enviado para " + clientAddress);
-                } else {
-                    controlWriter.println("ERROR: Arquivo não encontrado");
-                }
-            } else if (commandParts[0].equals("LIST")) {
-                controlWriter.println("OK");
-
-                // Cria um soquete de dados para o cliente
-                ServerSocket dataSocket = new ServerSocket(dataPort);
-                controlWriter.println(dataPort);
-
-                Socket dataConnection = dataSocket.accept();
-                PrintWriter dataWriter = new PrintWriter(dataConnection.getOutputStream(), true);
-
-                File directory = new File(currentDirectory);
-                File[] files = directory.listFiles();
-
-                for (File file : files) {
-                     dataWriter.println(file.getName());
-                }
-
-                dataWriter.close();
-                dataConnection.close();
-                dataSocket.close();
-
-                System.out.println("Lista de arquivos enviada para " + clientAddress);
-            } else if (commandParts[0].equals("PWD")) {
-                controlWriter.println("OK");  //TODO: refazer a lógica do PWD.
-
-                String currentDirectory = getCurrentDirectory();
-                controlWriter.println(currentDirectory); 
-            } else if (commandParts[0].equals("STOR")) {
-                String filename = commandParts[1];
-                controlWriter.println("OK");
-
-                String directory = commandParts[2];
-                File file = new File(directory + "\\" + filename);
-
-                // Cria um soquete de dados para o cliente
-                ServerSocket dataSocket = new ServerSocket(dataPort);
-                controlWriter.println(dataPort);
-
-                Socket dataConnection = dataSocket.accept();
-                BufferedInputStream dataReader = new BufferedInputStream(dataConnection.getInputStream());
-
-                FileOutputStream fileWriter = new FileOutputStream(file);
-
-                byte[] buffer = new byte[1024];
-                int bytesRead;
-
-                while ((bytesRead = dataReader.read(buffer)) != -1) {
-                    fileWriter.write(buffer, 0, bytesRead);
-                }
-
-                fileWriter.close();
-                dataReader.close();
-                dataConnection.close();
-                dataSocket.close();
-
-                System.out.println("Arquivo '" + filename + "' recebido e salvo em: " + file.getAbsolutePath());
-            } else {
-                controlWriter.println("ERROR: Comando inválido");
+            
+            boolean connectionClose = false;
+            while (!connectionClose) {
+	            String command = controlReader.readLine();
+	            String[] commandParts = command.split(" ");
+	            String line;
+	            
+	            if (commandParts[0].equals("RETR")) {
+	                String filename = commandParts[1];
+	                File file = new File(dirAtual+"\\"+ filename);
+	                if (file.exists()) {
+	                    controlWriter.println("OK");
+	                    // Cria um soquete de dados para o cliente
+	                    ServerSocket dataSocket = new ServerSocket(dataPort);
+	                    controlWriter.println(dataPort);
+	                    Socket dataConnection = dataSocket.accept();
+	                    BufferedOutputStream dataWriter = new BufferedOutputStream(dataConnection.getOutputStream());
+	                    FileInputStream fileReader = new FileInputStream(file);
+	                    byte[] buffer = new byte[1024];
+	                    int bytesRead;
+	                    while ((bytesRead = fileReader.read(buffer)) != -1)
+	                        dataWriter.write(buffer, 0, bytesRead);
+	                    fileReader.close();
+	                    dataWriter.flush();
+	                    dataWriter.close();
+	                    dataConnection.close();
+	                    dataSocket.close();
+	                    System.out.println("Arquivo '" + filename + "' enviado para " + clientAddress);
+	                } else controlWriter.println("ERROR: Arquivo não encontrado");
+	            }else if (commandParts[0].equals("LIST")) {
+	                controlWriter.println("OK");
+	                // Cria um soquete de dados para o cliente
+	                ServerSocket dataSocket = new ServerSocket(dataPort);
+	                controlWriter.println(dataPort);
+	                Socket dataConnection = dataSocket.accept();
+	                PrintWriter dataWriter = new PrintWriter(dataConnection.getOutputStream(), true);
+	                File[] files = dirAtual.listFiles();
+	                for (File file : files)
+	                    dataWriter.println(file.getName());
+	                dataWriter.close();
+	                dataConnection.close();
+	                dataSocket.close();
+	                System.out.println("Lista de arquivos enviada para " + clientAddress);
+	            }else if(commandParts[0].equals("STOR")) {
+	                controlWriter.println("OK");
+	                File file = new File(commandParts[2] + "\\" + commandParts[1]);
+	                System.out.println(file.toString());
+	                // Cria um soquete de dados para o cliente
+	                ServerSocket dataSocket = new ServerSocket(dataPort);
+	                controlWriter.println(dataPort);
+	                Socket dataConnection = dataSocket.accept();
+	                BufferedInputStream dataReader = new BufferedInputStream(dataConnection.getInputStream());
+	                FileOutputStream fileWriter = new FileOutputStream(file);
+	                byte[] buffer = new byte[1024];
+	                int bytesRead;
+	                while ((bytesRead = dataReader.read(buffer)) != -1) 
+	                    fileWriter.write(buffer, 0, bytesRead);
+	                fileWriter.close();
+	                dataReader.close();
+	                dataConnection.close();
+	                dataSocket.close();
+	                System.out.println("Arquivo '" + commandParts[1] + "' recebido e salvo em: " + file.getAbsolutePath());
+	            }else if (commandParts[0].equals("PWD")) {
+	                controlWriter.println("OK");
+	                String currentDirectory = dirAtual.getAbsolutePath();
+	                controlWriter.println(currentDirectory);
+	            }else if (commandParts[0].equals("CWD")) {
+	            	String[] arquivos = dirAtual.list();
+	            	List listaArquivo = Arrays.asList(arquivos);
+	            	if (listaArquivo.contains(commandParts[1])) {
+						File novoDir = new File(dirAtual + "\\" + commandParts[1]);
+						String caminhoNovo = novoDir.getAbsolutePath();
+						if (novoDir.isDirectory())
+							dirAtual = novoDir;
+	            	}
+	                controlWriter.println("OK");
+	                controlWriter.println(dirAtual); 
+	            }else if (commandParts[0].equals("CDUP")) {
+					File novoDir = new File(dirAtual.getParent());
+					dirAtual = novoDir;
+					controlWriter.println("OK");
+					controlWriter.println(dirAtual); 
+	            }else if (commandParts[0].equals("QUIT")) connectionClose = true;
+	            else controlWriter.println("ERROR: Comando inválido");
             }
-
             controlSocket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public String getCurrentDirectory() {
-        Path currentPath = Paths.get("");
-        return currentPath.toAbsolutePath().toString();
-    }
+        } catch (IOException e) {e.printStackTrace();} 
+   }
 }
